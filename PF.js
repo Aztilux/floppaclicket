faaa24c84c7b913fd37f3117ce1388f86d9ac481
Aztilux <82147183+Aztilux@users.noreply.github.com>
2022-01-25 16:58:47 +0100

    Add files via upload

--- PF.js
+++ PF.js
@@ -1,6 +1,10 @@
 var floppas = 0;
 var fishecost = 15;
 var fishenumber = 0;
+var floppercost = 100;
+var floppernumber = 0;
+var chikenmcost = 500;
+var chikenmnumber = 0;
 
 function floppers(amount) {
 	floppas = floppas + amount; 
@@ -12,7 +16,7 @@ function fishebuy() {
 	if (floppas >= fishecost) {
 		floppas = floppas - fishecost;
 		fishenumber = fishenumber + 1; 
-		fishecost = Math.round(fishecost * 2);
+		fishecost = Math.round(fishecost * 1.3);
 
 		document.getElementById("floppas").innerHTML = floppas;
 		document.getElementById("fishenumber").innerHTML = fishenumber;
@@ -20,7 +24,36 @@ function fishebuy() {
 	}
 }
 
+function flopperbuy() {
+	debugger;
+	if (floppas >= floppercost) {
+		floppas = floppas - floppercost;
+		floppernumber = floppernumber + 1; 
+		floppercost = Math.round(floppercost * 1.4);
+
+		document.getElementById("floppas").innerHTML = floppas;
+		document.getElementById("floppernumber").innerHTML = floppernumber;
+		document.getElementById("floppercost").innerHTML = floppercost;
+	}
+}
+
+function chikenmbuy() {
+	debugger;
+	if (floppas >= chikenmcost) {
+		floppas = floppas - chikenmcost;
+		chikenmnumber = chikenmnumber + 1; 
+		chikenmcost = Math.round(chikenmcost * 1.5);
+
+		document.getElementById("floppas").innerHTML = floppas;
+		document.getElementById("chikenmnumber").innerHTML = chikenmnumber;
+		document.getElementById("chikenmcost").innerHTML = chikenmcost;
+	}
+}
+
 setInterval (function() {
 	floppas = floppas + fishenumber;
+	floppas = floppas + floppernumber * 3;
+	floppas = floppas + chikenmnumber * 10;
+
 	document.getElementById("floppas").innerHTML = floppas;
 }, 1000);
